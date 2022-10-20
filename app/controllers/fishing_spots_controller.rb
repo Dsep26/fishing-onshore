0397aa89bce5dcbf8186f8095dd93e35e61b0ff4
Nuzhah Kureemun <nuzk2904@gmail.com>
2022-10-20 16:01:32 +0400

    add link to pages and some css to fishing spot index

--- app/controllers/fishing_spots_controller.rb
+++ app/controllers/fishing_spots_controller.rb
@@ -3,18 +3,18 @@ class FishingSpotsController < ApplicationController
 
   def index
     @fishingspots = FishingSpot.all
-    
+
   end
 
   def new
     @fishingspots = FishingSpot.new(fish_params)
     @fishingspot = FishingSpot.save(set_spot)
 
-  end  
+  end
 
   def show
     @fishingspots = FishingSpot.geocoded
-    @marker = [{ 
+    @marker = [{
       lat: @fishingspot.latitude,
       lng: @fishingspot.longitude,
       image_url: helpers.asset_url("logo.png")
@@ -26,6 +26,7 @@ class FishingSpotsController < ApplicationController
   def set_spot
     @fishingspot = FishingSpot.find(params[:id])
   end
+  
   def fish_params
     params.require(:fishingspots).permit(:address, :fishing_activity, :longitude, :latitude)
   end
